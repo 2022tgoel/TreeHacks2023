b156f04e91cc5cb4dd419d2bfb449bef7adf8bce
2022tgoel <tishi.tng@gmail.com>
2023-02-18 19:02:00 -0500

    more neural netw stuff working

--- client/src/components/pages/NeuralNet.js
+++ client/src/components/pages/NeuralNet.js
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 import "../../utilities.css";
 import "./NeuralNet.css"
 
-let s = 54.8; /* hardcoded */
+let s = 120; /* hardcoded */
+let r = 35 
 
 const Layer = (props) => {
   const [value, setValue] = useState(0);
@@ -28,69 +29,68 @@ const Layer = (props) => {
   );
 }
 
-// const Edge = (props) => { // edges are positioned relative to layers 
-//   let c = props.coordinates;
-//   console.log(c);
-//   return (<svg position="absolute" width="500" height="500"><line x1={s/2} y1={s/2} x2={s/2+c[0]} y2={s/2+c[1]} stroke="black"/></svg>);
-// }
+function rgbToHex(r, g, b) {
+  return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
+
+const Edge = (props) => { // edges are positioned relative to layers 
+  let cx = (props.x1 + props.x2) / 2;
+  let cy = (props.y1 + props.y2) / 2
+  return (
+  <>
+    <g>
+      <line className="edge" x1={props.x1} y1={props.y1} x2={props.x2} y2={props.y2} stroke={rgbToHex(120 + 30*props.weight, 0, 0)} strokeWidth={5}/>
+      <circle className="btn" cx={cx-7} cy={cy} r={5} onClick={() => props.increaseWeight()}></circle>
+      <circle className="btn" cx={cx+7} cy={cy} r={5} onClick={() => props.decreaseWeight()}></circle>
+    </g>
+    
+  </>);
+  
+}
 
 class NeuralNet extends React.Component {
   constructor(props){
     super(props);
     this.n = 3;
-    this.state = {layer_sizes : Array(this.n).fill(1)}; // two layers, each with size 1
-    
+    let layer_sizes = Array(this.n).fill(1)
+    this.state = {layer_sizes : layer_sizes, weights : this.resetWeights(layer_sizes)}; // two layers, each with size 1
+    this.data = [[0, 0, 0], [1, 0, 1], [0, 1, 1], [1, 1, 0]];
+  }
+
+  resetWeights(layer_sizes){
+    console.log("resetting weights");
+    let weights = Array();
+    for(let layer = 0; layer < this.n-1; layer++){
+      // out_dim by in_dim matrix
+      let mat = Array(layer_sizes[layer+1]);
+      console.log("hellow", layer_sizes, layer, layer+1);
+      for (let i = 0; i < layer_sizes[layer+1]; i++){
+        mat[i] = Array(layer_sizes[layer]).fill(1);
+      }
+      weights.push(mat);
+    }
+    return weights;
   }
 
   handleSubmit(layer_num, value){
     let state = this.state.layer_sizes.slice();
-    state[layer_num] = value;
-    this.setState({layer_sizes : state});
+    state[layer_num] = parseInt(value);
+    this.setState({layer_sizes : state, weights : this.resetWeights(state)})
   }
 
-  // getEdgeCoordinates(layer, n1, n2){ // relative to the first node, get what the diff should be for the second coord
-  //   let height = Math.max(...this.state.layer_sizes) * s; 
-  //   let h1 = n1*s + s/2 + height/2 - s*this.state.layer_sizes[layer]/2;
-  //   let h2 = n2*s + s/2 + height/2 - s*this.state.layer_sizes[layer+1]/2;
-  //   return [s, h2-h1];
-  // }
+  increaseWeight(layer, i, j){
+    let weights = this.state.weights.slice();
+    weights[layer][j][i]+=1;
+    console.log(weights[layer][j][i]);
+    this.setState({weights :weights});
+  }
 
-  // getEdge(layer, n1, n2){
-  //   let coord = this.getEdgeCoordinates(layer, n1, n2);
-  //   return <><Edge coordinates={coord} key={n1*n2}></Edge></>
-  // }
-
-  // getNode(layer_num, n1){ // put all the edges
-  //   console.log("building node", layer_num, n1);
-  //   let edges = Array();
-  //   console.log("here");
-  //   if (layer_num == this.n-1) return <node>{edges}</node>;
-  //   console.log(layer_num, n1);
-  //   for (let j = 0; j < this.state.layer_sizes[layer_num+1]; j++){
-  //     console.log("building edge", layer_num, n1, j);
-  //     edges.push(this.getEdge(layer_num, n1, j));
-  //   }
-  //   return <node key={n1}>{edges}</node>;
-  // }
-
-  // getLayer(layer_num){
-  //   let n = this.state.layer_sizes[layer_num];
-  //   let nodes = Array(n);
-  //   for (let i = 0;i < n; i++){
-  //     nodes[i] = this.getNode(layer_num, i);
-  //   }
-  //   return <><layer key={layer_num}>{nodes}</layer></>;
-  // }
-
-  // getLayers(){
-  //   console.log("building layers");
-  //   let n = this.n;
-  //   let l = Array(n);
-  //   for (let i = 0; i < n; i++){
-  //     l[i] = this.getLayer(i);
-  //   }
-  //   return <>{l}</>
-  // }
+  decreaseWeight(layer, i, j){
+    let weights = this.state.weights.slice();
+    weights[layer][j][i]-=1;
+    console.log(weights[layer][j][i]);
+    this.setState({weights :weights});
+  }
 
   getSVGForNetwork(){
     let height = s*Math.max(...this.state.layer_sizes)
@@ -101,8 +101,8 @@ class NeuralNet extends React.Component {
       let x = s*layer + s/2; // center of all the circles
       for (let node =0; node < this.state.layer_sizes[layer]; node++){
         let y = node*s + s/2 + height/2 - s*this.state.layer_sizes[layer]/2;
-        console.log(key, x, y);
-        components.push(<circle key={key} cx={x} cy={y} r={20}></circle>);
+        // console.log(key, x, y);
+        components.push(<circle key={key} cx={x} cy={y} r={r}></circle>);
         key+=1;
       }
     }
@@ -112,14 +112,13 @@ class NeuralNet extends React.Component {
         for (let j =0; j < this.state.layer_sizes[layer+1]; j++){
           let y1 = i*s + s/2 + height/2 - s*this.state.layer_sizes[layer]/2;
           let y2 = j*s + s/2 + height/2 - s*this.state.layer_sizes[layer+1]/2;
-          console.log(y1, y2);
-          components.push(<line key={key} x1={s*layer+s/2} x2={s*(layer+1)+s/2} y1={y1} y2={y2} stroke="black"></line>)
+          // console.log(y1, y2);
+          components.push(<Edge key={key} x1={s*layer+s/2} x2={s*(layer+1)+s/2} y1={y1} y2={y2} weight={this.state.weights[layer][j][i]} increaseWeight={()=> this.increaseWeight(layer, i, j)} decreaseWeight={()=> this.decreaseWeight(layer, i, j)}></Edge>)
           key++
         }
       }
     }
 
-    print(components);
     return (<><svg position="absolute" width={s*this.n} height={height}>{components}</svg></>);
   }
 
@@ -132,17 +131,37 @@ class NeuralNet extends React.Component {
     return <>{layerInps}</>
   }
 
+  getTable(){
+    let table = Array();
+    table.push(<><tr>
+      <th>Input 1</th>
+      <th>Input 2</th>
+      <th>Output</th>
+    </tr></>);
+    for (let i = 0; i < 4; i++){
+      table.push(<><tr>
+        <th>{this.data[i][0]}</th>
+        <th>{this.data[i][1]}</th>
+        <th>{this.data[i][2]}</th>
+      </tr></>)
+    }
+    return <data><table>{table}</table></data>;
+  }
+
   render(){
+    console.log(this.state.weights);
     return (
       <>
-      <h1>Hello</h1>
+      <h1>Hand Train a Model for XOR</h1>
       <page>
+        {this.getTable()}
         <layersinp>
-          Hello, this is where you specify the network <br></br>
+          Specify the architecture of the network<br></br>
+          The input layer should have two dimensions <br></br>
+          and the output layer should have one dimension <br></br>
           {this.getLayersInp()}
         </layersinp>
         <network>
-          Hi this is the network
           {this.getSVGForNetwork()}
         </network>
       </page>
