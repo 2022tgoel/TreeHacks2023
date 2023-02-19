07bd73042b87466d9eca94d68404a247259c6d50
Aarush Gupta <aarush.g75@gmail.com>
2023-02-19 08:31:20 -0800

    Added homepage

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Background from "../../images/Homepage.png";
+import NNL from "../../images/NNL.png";
+import MIABL from "../../images/MIABL.png";
 
 import "./style.css"
 const HomePage = () => {
   return (
-    <div></div>
+    <div style={{width: "100vw", height: "105vh", backgroundImage: `url(${Background})`, backgroundSize: "cover", marginTop: "-5vh"}}>
+      <div style={{position: "absolute", top: "32vh", left: "30vw"}}>
+        <img src={NNL} onClick={() => { window.location.href = "/xornet"}} style={{width: "25vw"}}></img><br></br>
+        <img src={MIABL} onClick={() => { window.location.href = "/pca"}} style={{width: "25vw"}}></img>
+      </div>
+    </div>
   );
 };
 
